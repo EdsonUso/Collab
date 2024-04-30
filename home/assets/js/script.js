69f80d5a02ba9ed1a9c2e09ee89d196187f84460
EdsonUso <edson.cruz@sptech.school>
2024-04-30 14:01:55 -0300

    dinamismo no modal

--- home/assets/js/script.js
+++ home/assets/js/script.js
@@ -1,3 +1,4 @@
+//Função para detecção do tema
 function detectionColor() {
     var theme = "dark" //padrão
 
@@ -18,9 +19,9 @@ function detectionColor() {
 }
 
 detectionColor();
-    
+
 const showModalPerfil = () => {
-    const modal = document.getElementById('modal-papel');
+    const modal = document.getElementById('modal_papel');
 
     modal.showModal();
 
@@ -34,9 +35,55 @@ const showModalPerfil = () => {
 
 showModalPerfil();
 
+const optionProgramador = ['inciante', 'estudante', 'profissional', 'pleno', 'senior']
+const optionDesigner = ['iniciante', 'estudante', 'profissional', 'acima'];
 
-function setRole(role){
-    const modal = document.getElementById('modal-papel');
+function setRole(role) {
+    //pegando o modal novamente ('talvez a logica não esteja das melhores')
+    const modal = document.getElementById('modal_papel')
 
-    modal.innerHTML = ``
+    //pegando o  form modal e colocando em uma variavel
+    const form = document.getElementById('area_form')
+
+    console.log(role)
+
+    //criando o botão de dentro do modal dinamicamente
+    const divButton = document.createElement('div')
+    const nextButton = document.createElement('button');
+    const textButton = document.createTextNode('Proximo')
+
+    //Adicionando um id ao botão 
+
+    //Adicionando classes para os elementos criados dinamicamente
+    divButton.classList.add('area-button-next');
+    nextButton.classList.add('modal-button-next');
+
+    //inserindo os novos elementos no 
+
+    if (document.querySelector('.modal-button-next') == null ) {
+        nextButton.appendChild(textButton)
+        form.appendChild(divButton);
+        divButton.appendChild(nextButton);
+    }
+
+
+    nextButton.addEventListener('click', ()=>{
+        modal.innerHTML = '';
+
+        const divCards = document.createElement('div');
+        const card = document.createElement('div');
+
+        card.classList.add('card')
+
+        
+        
+
+    })
+
+    
 }
+
+
+
+
+
